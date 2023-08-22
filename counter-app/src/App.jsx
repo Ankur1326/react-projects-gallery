d29a69497b54eeab233745c914cf870d936ce9ae
Ankur1326 <swamiankur46@gmail.com>
2023-08-22 11:20:22 +0530

    update App.jsx and App.css

--- counter-app/src/App.jsx
+++ counter-app/src/App.jsx
@@ -1,14 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [counter, setCounter] = useState(0)
 
+  const Increment = () => {
+    if (counter < 20) {
+      setCounter(counter + 1)
+    } else {
+      window.alert("Value cannot be greter than zero")
+    }
+  }
+  const Decrement = () => {
+    if (counter > 0) {
+      setCounter(counter - 1)
+    } else {
+      window.alert("Value cannot be less than zero")
+    }
+  }
+  
   return (
     <>
-      
+      <h1>React Counter App</h1>
+      <p>Count : <span className='count'>{counter}</span></p>
+      <button onClick={Decrement}>Dec</button>
+      <button onClick={Increment}>Inc</button>
     </>
   )
 }
