50bea76e78881f5d52d90edd6af867f42f297bc8
Ankur1326 <swamiankur46@gmail.com>
2023-09-05 14:22:21 +0530

    add search

---
+++ POKEDEX/src/components/search/Search.jsx
@@ -0,0 +1,13 @@
+import './Search.css'
+function Search() {
+    return (
+        <div className="input-wrapper">
+            <input 
+                type="text"
+                placeholder="pokemon search"
+            />
+        </div>
+    );
+}
+
+export default Search;
