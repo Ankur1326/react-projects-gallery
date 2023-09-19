34a2780df3f3e73d8bab54c8daf4717202f95026
Ankur1326 <swamiankur46@gmail.com>
2023-09-19 06:57:51 +0530

    app.js complete

--- miniContext/src/App.jsx
+++ miniContext/src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
-import UserContextProvider from './context/UserContectProvider'
+import Login from './components/Login'
+import Profile from './components/Profile'
+import UserContextProvider from './context/UserContextProvider'
 
 function App() {
 
   return (
     <UserContextProvider>
-        
+      <h1>React with Chai and share is important</h1>
+      <Login />
+      <Profile />
     </UserContextProvider>
   )
 }
