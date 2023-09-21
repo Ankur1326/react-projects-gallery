c92c303f09394f150b3ca47b2a1b344338a8a421
Ankur1326 <swamiankur46@gmail.com>
2023-09-21 14:31:14 +0530

    contexts

---
+++ todoContextLocalStorage/src/contexts/todoContext.js
@@ -0,0 +1,17 @@
+import { createContext, useContext } from "react";
+
+export const TodoContext = createContext({
+    todos: [
+        {
+            id: 1,
+            todo: "Todo msg",
+            completed: false,
+        }
+    ],
+    addTodo: () => {},
+    updatedTodo: (id, todo) => {},
+    deleteTodo: (id) => {},
+    toggleComplete: (id) => {},
+})
+
+export const useTodo = () => {}
