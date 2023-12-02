0fa224d91b2318a6e20602954999088f067cea08
Ankur1326 <swamiankur46@gmail.com>
2023-12-02 16:41:07 +0530

    interview

--- counter-app/src/App.jsx
+++ counter-app/src/App.jsx
@@ -1,32 +1,39 @@
-import { useState } from 'react';
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [counter, setCounter] = useState(0)
+  const [counter, setCounter] = useState(0);
 
   const Increment = () => {
     if (counter < 20) {
-      setCounter(counter + 1)
+      setCounter(counter + 1);
+
+      // setCounter(counter => counter + 1);
+      // setCounter(counter => counter + 1);
+      // setCounter(counter => counter + 1);
+      // setCounter(counter => counter + 1);
     } else {
-      window.alert("Value cannot be greter than zero")
+      window.alert("Value cannot be greter than 20");
     }
-  }
+  };
   const Decrement = () => {
     if (counter > 0) {
-      setCounter(counter - 1)
+      setCounter(counter - 1);
     } else {
-      window.alert("Value cannot be less than zero")
+      window.alert("Value cannot be less than zero");
     }
-  }
-  
+  };
+
   return (
     <>
       <h1>React Counter App</h1>
-      <p>Count : <span className='count'>{counter}</span></p>
+      <p>
+        Count : <span className="count">{counter}</span>
+      </p>
       <button onClick={Decrement}>Dec</button>
       <button onClick={Increment}>Inc</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
